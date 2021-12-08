4522b50afab0f25030d46cb5b8fc14f6583af92c
YungPingXu <a0976373843@gmail.com>
2021-12-08 22:00:17 +0800

    add database usage

---
+++ database/db_create_table.py
@@ -0,0 +1,24 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+### don't touch ###
+load_dotenv()
+database = os.getenv("database")
+user = os.getenv("user")
+password = os.getenv("password")
+host= os.getenv("host")
+port = os.getenv("port")
+
+conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+cur = conn.cursor()
+###################
+
+cur.execute("""
+    CREATE TABLE test2(
+        col1 TEXT,
+        col2 TEXT
+    );
+""")
+conn.commit()
+conn.close()
