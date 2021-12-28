49896b1e9454d7ab04757ae2923c3ea4aeea3e67
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2021-12-28 11:30:49 +0800

    adding other responds

--- index.py
+++ index.py
@@ -40,6 +40,7 @@ def handle_message(event):
     user_message = event.message.text
     print(event.message)
     reply_message = "@" + user_name + "your ID:" + user_id + "\n Testing!!! \n 您傳送的訊息為：\n" + user_message
+    respond = "@昱瑋可以趕快填時間嗎?"
     #line_bot_api.reply_message(event.reply_token, TextMessage(text=reply_message))
     
     message = TemplateSendMessage(
@@ -63,7 +64,6 @@ def handle_message(event):
     if user_message == "@bot":
         line_bot_api.reply_message(event.reply_token, message)
     elif user_message == "total":
-        respond = "@昱瑋可以趕快填時間嗎?"
         line_bot_api.reply_message(event.reply_token, respond)
 
 
