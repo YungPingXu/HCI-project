2bc289c2ae63544d9c6419628954ad2750818b39
YungPingXu <a0976373843@gmail.com>
2022-01-07 19:52:53 +0800

    add start_time and end_time

--- database/db_utils.py
+++ database/db_utils.py
@@ -473,12 +473,12 @@ def select_event_id(event_id):
         event_attribute['end_date'] = str(row[3])
         event_attribute['start_time'] = time.strftime(row[4], '%H:%M')
         event_attribute['end_time'] = time.strftime(row[5], '%H:%M')
-        #event_attribute['deadline_date'] = time.strftime(row[6], '%Y-%m-%d')
-        #event_attribute['deadline_time'] = time.strftime(row[7], '%H:%M:%S')
-        #event_attribute['anonymous'] = row[8]
-        #event_attribute['preference'] = row[9]
-        #event_attribute['have_must_attend'] = row[10]
-        #event_attribute['group_id'] = row[11]
+        event_attribute['deadline_date'] = time.strftime(row[6], '%Y-%m-%d')
+        event_attribute['deadline_time'] = time.strftime(row[7], '%H:%M:%S')
+        event_attribute['anonymous'] = row[8]
+        event_attribute['preference'] = row[9]
+        event_attribute['have_must_attend'] = row[10]
+        event_attribute['group_id'] = row[11]
 
     conn.commit()
     conn.close()
