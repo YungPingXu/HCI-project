db04f1bc4f8999c5def79a23aa7fd1f0d6261c62
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-01-09 01:06:01 +0800

    add the normal result panel

--- index.py
+++ index.py
@@ -63,11 +63,14 @@ def handle_message(event):
     elif message == "botdone":
         FlexMessage = json.load(open('voting_time.json', 'r', encoding='utf-8'))
         line_bot_api.reply_message(event.reply_token, FlexSendMessage('profile', FlexMessage))
-    elif message == "nocommon":
+    elif message == "no_common":
         FlexMessage = json.load(open('no_common.json', 'r', encoding='utf-8'))
         line_bot_api.reply_message(event.reply_token, FlexSendMessage('profile', FlexMessage))
+    elif message == "everyone_ok_result":
+        FlexMessage = json.load(open('eneryone_ok_result.json', 'r', encoding='utf-8'))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage('profile', FlexMessage))
     elif message == "result":
-        FlexMessage = json.load(open('result.json', 'r', encoding='utf-8'))
+        FlexMessage = json.load(open('normal_result.json', 'r', encoding='utf-8'))
         line_bot_api.reply_message(event.reply_token, FlexSendMessage('profile', FlexMessage))
     elif message == "hihi":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="I'm here !! :)"))
